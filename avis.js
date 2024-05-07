73ae0deaa37e6b003a1a8e7f5261abbad4781a24
xavmllt <xaviermaillot100@gmail.com>
2024-05-07 02:29:55 +0200

    Saving data in localStorage

--- avis.js
+++ avis.js
@@ -9,19 +9,22 @@ export function ajoutListenersAvis() {
         const id = event.target.dataset.id;
         const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
         const avis = await reponse.json();
+        window.localStorage.setItem(`avis-piece-${id}`, JSON.stringify(avis))
         const pieceElement = event.target.parentElement;
-
-        const avisElement = document.createElement("p");
-        for (let i = 0; i < avis.length; i++) {
-            avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
-        }
-        pieceElement.appendChild(avisElement);
- 
+        afficherAvis(pieceElement, avis)
      });
  
     }
  }
  
+ export function afficherAvis(pieceElement, avis){
+    const avisElement = document.createElement("p");
+    for (let i = 0; i < avis.length; i++) {
+        avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
+    }
+    pieceElement.appendChild(avisElement);
+ }
+
  export function ajoutListenerEnvoyerAvis() {
     const formulaireAvis = document.querySelector(".formulaire-avis");
     formulaireAvis.addEventListener("submit", function (event) {
@@ -29,9 +32,9 @@ export function ajoutListenersAvis() {
     // Création de l’objet du nouvel avis.
     const avis = {
         pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
-        utilisateur: event.target.querySelector("[name=utilisateur").value,
+        utilisateur: event.target.querySelector("[name=utilisateur]").value,
         commentaire: event.target.querySelector("[name=commentaire]").value,
-        nbEtoiles: parseInt(event.target.querySelector("[name=nb-etoiles]").value)
+        nbEtoiles: parseInt(event.target.querySelector("[name=nbEtoiles]").value)
     };
     // Création de la charge utile au format JSON
     const chargeUtile = JSON.stringify(avis);
--- pieces.js
+++ pieces.js
@@ -1,8 +1,18 @@
-import { ajoutListenersAvis, ajoutListenerEnvoyerAvis } from "./avis.js";
+import { ajoutListenersAvis, ajoutListenerEnvoyerAvis, afficherAvis } from "./avis.js";
+//Récupération des pièces eventuellement stockées dans le localStorage
+let pieces = window.localStorage.getItem('pieces');
 
-// Récupération des pièces depuis le fichier JSON
-const reponse = await fetch('http://localhost:8081/pieces/');
-const pieces = await reponse.json();
+if (pieces === null){
+    // Récupération des pièces depuis l'API
+    const reponse = await fetch('http://localhost:8081/pieces/');
+    pieces = await reponse.json();
+    // Transformation des pièces en JSON
+    const valeurPieces = JSON.stringify(pieces);
+    // Stockage des informations dans le localStorage
+    window.localStorage.setItem("pieces", valeurPieces);
+}else{
+    pieces = JSON.parse(pieces);
+}
 // on appel la fonction pour ajouter le listener au formulaire
 ajoutListenerEnvoyerAvis()
 
@@ -14,6 +24,7 @@ function genererPieces(pieces){
         const sectionFiches = document.querySelector(".fiches");
         // Création d’une balise dédiée à une pièce automobile
         const pieceElement = document.createElement("article");
+        pieceElement.dataset.id = pieces[i].id
         // Création des balises 
         const imageElement = document.createElement("img");
         imageElement.src = article.image;
@@ -49,7 +60,18 @@ function genererPieces(pieces){
 
 genererPieces(pieces);
 
- //gestion des bouttons 
+for(let i = 0; i < pieces.length; i++){
+    const id = pieces[i].id;
+    const avisJSON = window.localStorage.getItem(`avis-piece-${id}`);
+    const avis = JSON.parse(avisJSON);
+
+    if(avis !== null){
+        const pieceElement = document.querySelector(`article[data-id="${id}"]`);
+        afficherAvis(pieceElement, avis)
+    }
+}
+
+ //gestion des boutons 
 const boutonTrier = document.querySelector(".btn-trier");
 
 boutonTrier.addEventListener("click", function () {
@@ -117,7 +139,6 @@ document.querySelector('.abordables')
     .appendChild(pElement)
     .appendChild(abordablesElements);
 
-//Code Exercice 
 const nomsDisponibles = pieces.map(piece => piece.nom)
 const prixDisponibles = pieces.map(piece => piece.prix)
 
@@ -148,3 +169,9 @@ inputPrixMax.addEventListener('input', function(){
     document.querySelector(".fiches").innerHTML = "";
     genererPieces(piecesFiltrees);  
 })
+
+// Ajout du listener pour mettre à jour des données du localStorage
+const boutonMettreAJour = document.querySelector(".btn-maj");
+boutonMettreAJour.addEventListener("click", function () {
+   window.localStorage.removeItem("pieces");
+});
